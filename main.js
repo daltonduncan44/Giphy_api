7f82eedc0c04b822c5118c2b01f98ac86fd2ce60
Dalton Duncan <daltonduncan44@gmail.com>
2018-07-30 19:49:27 -0400

    hard restet but it works

--- main.js
+++ main.js
@@ -1,33 +1,74 @@
+var topics = ["It","lion king","step brothers","troy", "iron man","top gun","happy gilmore"];
+var numberOfGIFs = 10;
+var cutOffRating = "PG";
+var apiKey= "gQeBWleLEAwNNxxJTV5wnK0YZO6PK1lD"
+
+function renderButtons(){
+	for(var i = 0; i < topics.length; i++) {
+		var newButton = $("<button>");
+		newButton.addClass("btn");
+		newButton.addClass("newMovieButton");
+		newButton.text(topics[i]);
+		$("#button-container").append(newButton);
+	}
+	$(".newMovieButton").unbind("click");
+
+	$(".newMovieButton").on("click", function(){
+		$(".gif-image").unbind("click");
+		$("#gif-container").empty();
+		$("#gif-container").removeClass("dotted-border");
+		populateGIFContainer($(this).text());
+	});
+
+}
+
+function addButton(show){
+	if(topics.indexOf(show) === -1) {
+		topics.push(show);
+		$("#button-container").empty();
+		renderButtons();
+	}
+}
+
+function populateGIFContainer(show){
+	$.ajax({
+		url: "https://api.giphy.com/v1/gifs/search?q=" + show + 
+		"&api_key=gQeBWleLEAwNNxxJTV5wnK0YZO6PK1lD&rating=" + cutOffRating + "&limit=" + numberOfGIFs,
+		method: "GET"
+	}).then(function(response){
+		response.data.forEach(function(element){
+			newDiv = $("<div>");
+			newDiv.addClass("individual-gif-container");
+			newDiv.append("<p>Rating: " + element.rating.toUpperCase() + "</p>");
+			var newImage = $("<img src = '" + element.images.fixed_height_still.url + "'>");
+			newImage.addClass("gif-image");
+			newImage.attr("state", "still");
+			newImage.attr("still-data", element.images.fixed_height_still.url);
+			newImage.attr("animated-data", element.images.fixed_height.url);
+			newDiv.append(newImage);
+			$("#gif-container").append(newDiv);
+		});
+		
+		$("#gif-container").addClass("dotted-border");
+		$(".gif-image").unbind("click");
+		$(".gif-image").on("click", function(){
+			if($(this).attr("state") === "still") {
+				$(this).attr("state", "animated");
+				$(this).attr("src", $(this).attr("animated-data"));
+			}
+			else {
+				$(this).attr("state", "still");
+				$(this).attr("src", $(this).attr("still-data"));
+			}
+		});
+	});
+}
+
 $(document).ready(function(){
-    console.log("ready to party")
-/// start array to post to doc
-var animalsArray = ["dog", "cat", "lizard", "monkey", "bird", "fish",];
-
-
-    // Deleting the movies prior to adding new movies so buttons dont repeat
-    function loadButtons() {
-    $("#buttons").empty();
-    
-    // Looping through the array of movies
-    for (var i = 0; i < animalsArray.length; i++) {
-
-        // Then dynamicaly generating buttons for each movie in the array
-        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-        var a = $("<a>");
-        // Adding a class
-        a.addClass("btn btn-info btn-sm button m-1 queryButton");
-        // Added a data-attribute
-        a.attr("data-name", animalsArray[i].replace(/\s+/g, '+'));
-        // Added a data-attribute
-        a.text(animalsArray[i]);
-        // Added the button to the HTML
-        $("#buttons").append(a);
-        $("#newbuttons").val();
-    }
-    };
-
-loadButtons();
-
-
-
+	renderButtons();
+	$("#submit").on("click", function(){
+		event.preventDefault();
+		addButton($("#movies").val().trim());
+		$("#movies").val("");
+	});
 });
